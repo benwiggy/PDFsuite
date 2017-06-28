ec1cb597915f273087e6a92ba9425010c98168fa
benwiggy <ben.bw@me.com>
2017-06-28 10:32:58 +0100

    New version

--- booklet.py
+++ booklet.py
@@ -1,8 +1,8 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
 # ----------------------------------------------------------------
 # PDF Booklet Imposition Script for MacOS
-# by Ben Byram-Wigfield v.1.1
+# by Ben Byram-Wigfield v.2.0
 # Feel free to use, modify and pass on with acknowledgement.
 
 # 1. Set OPTIONS below for output folder, sheet size, and creep
@@ -10,85 +10,50 @@
 # 3. It will then appear as an option in the PDF button of the Print dialog.
 # (if it has executable flags set.)
 
-# Script now checks for page rotation.
-# Still to do: 
-# 1. Allow for multiple booklets from one PDF file.
+# Script can be configured for stacking 4pp signatures or gathering booklet spreads.
 # ----------------------------------------------------------------
-import sys
-import os
+
+# Beware of indexes starting from 0 and 1...!!! CGPDFDocument starts page count at 1.
+
+import os, sys
 import copy
-import getopt
 import Quartz as Quartz
 from CoreFoundation import (CFURLCreateFromFileSystemRepresentation, kCFAllocatorDefault)
 
-# For debugging, turn this on.
-verbose = False
 
-# define Page and Sheet Sizes in points
-A4 = [[0,0], [841.88, 595.28]]
+# define Page and Sheet Sizes in points. Might need CGRect, but works as is.
 A3 = [[0,0], [1190.55, 841.88]]
+# A4 = [[0,0], [841.88, 595.28]]
+# USLetter = [[0,0], [792, 612]]
+# Tabloid = [[0,0], [1224, 792]]
 
-USLetter = [[0,0], [792, 612]]
-Tabloid = [[0,0], [1224, 792]]
-# ----------------------------------------------------------------
-# OPTIONS. Set the location for saving the files. Must end with a /
-destination = os.path.expanduser("~/Desktop/")
-# Set file suffix
-suffix = " booklet.pdf"
-
-creep = 0.5 # in points. NB: Eventually, the pages will collide.
-
+# OPTIONS
 # Change this to one of the sizes listed above, if you want.
 sheetSize = A3
-# ----------------------------------------------------------------
-# For future: non-zero value will make booklets no bigger than _signature
-signature = 0 
-# ----------------------------------------------------------------
-# Dont change this.
-pagesPerSheet = 4
-leftPage = copy.deepcopy(sheetSize)
-shift = sheetSize[1][0]/2
-leftPage[1][0] = shift
-rightPage = copy.deepcopy(leftPage)
-rightPage[0][0] = shift
-
+# Set the location for saving the files. 
+destination = os.path.expanduser("~/Desktop")
+# Set file suffix
+suffix = " booklet.pdf"
+# If hasSignatures, sheets will be arranged for stacking in 4pp sections.
+hasSignatures = False
+pagesPerSheet = 4 # Not sure what will happen if this is changed.
+creep = 0.5 # in points. NB: Eventually, the pages will collide.
+imposedOrder = [] 
 
 # FUNCTIONS
 
 def createPDFDocumentWithPath(path):
-	global verbose
-	if verbose:
-		print "Creating PDF document from file %s" % (path)
 	return Quartz.CGPDFDocumentCreateWithURL(CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, path, len(path), False))
 
-
-def pageCount(pdfPath):
-	global verbose
-	if verbose:
-		print "Getting number of pages..."
-		
-	pdf = Quartz.CGPDFDocumentCreateWithProvider(Quartz.CGDataProviderCreateWithFilename (pdfPath))
-	return pdf.getNumberOfPages()
-    
-def imposition(pages):
-	global verbose
-	blanks = 0
-	UnsortedOrder = range(1, pages+1)
-	if pages%pagesPerSheet:
-		blanks = pagesPerSheet - (pages%pagesPerSheet)
-		for i in range(blanks):
-			UnsortedOrder.append(0)
-	imposedOrder = []
-	for i in range(1, (pages+blanks)/2+1, 2):
+def imposition(pageRange):
+	for i in range(1, (len(pageRange)/2), 2):
 		# First we do recto
-		imposedOrder.append(UnsortedOrder[i*-1])
-		imposedOrder.append(UnsortedOrder[i-1])
+		imposedOrder.append(pageRange[i*-1])
+		imposedOrder.append(pageRange[i-1])
 		# And now we do verso
-		imposedOrder.append(UnsortedOrder[i])
-		imposedOrder.append(UnsortedOrder[(i+1)*-1])
+		imposedOrder.append(pageRange[i])
+		imposedOrder.append(pageRange[(i+1)*-1])
 
-	if verbose:
-		print imposedOrder	
 	return imposedOrder
 
 def getRotation(pdfpage):
@@ -100,37 +65,51 @@ def getRotation(pdfpage):
 		y = Quartz.CGRectGetHeight(mediaBox)
 		if (x > y): displayAngle = -90
 		displayAngle -=  rotValue
-		print(displayAngle, rotValue)
 	return displayAngle	
 
 def contextDone(context):
 	if context:
 		Quartz.CGPDFContextClose(context)
-		del context
-		
+		del context	
+
 # MAIN 
 def main(argv):
-	(title, options, pathToFile) = argv[0:3]
-	global verbose
-	writeContext = None
+	(title, options, pathToFile) = argv[:]
 	shortName = os.path.splitext(title)[0]
-	writeFilename = destination + shortName + suffix
+	writeFilename = os.path.join(destination, shortName + suffix)
+	leftPage = copy.deepcopy(sheetSize)
+	shift = sheetSize[1][0]/2
+	leftPage[1][0] = shift
+	rightPage = copy.deepcopy(leftPage)
+	rightPage[0][0] = shift
+	blanks = 0
+
+# Initiate new PDF, get source PDF data, number of pages.
 	writeContext = Quartz.CGPDFContextCreateWithURL(CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, writeFilename, len(writeFilename), False), sheetSize, None)
-
-# Initiate new PDF, get source PDF, number of pages.
 	source = createPDFDocumentWithPath(pathToFile)
-	pageNo = Quartz.CGPDFDocumentGetNumberOfPages(source)
+	totalPages = Quartz.CGPDFDocumentGetNumberOfPages(source)
 
-# Get imposed page order
-	imposedOrder = imposition(pageNo)
 
-# Place pages on sheet
-	pagesPerSide = pagesPerSheet/2
-	Sides = len(imposedOrder) / pagesPerSide
+# Add blank pages to round up to multiple of pages per sheet.
+	UnsortedOrder = range(1, totalPages+1)
+	if totalPages%pagesPerSheet:
+		blanks = pagesPerSheet - (totalPages%pagesPerSheet)
+		for i in range(blanks):
+			UnsortedOrder.append(0)
+	totalPages = len(UnsortedOrder)
+
+	if hasSignatures:
+		signatureSize = pagesPerSheet
+	else:
+		signatureSize = totalPages
+
+	for something in range(0, totalPages, signatureSize):
+		imposition(UnsortedOrder[something:(something+signatureSize)])
 
 # For each side of the sheet, we must...
 # ... create a PDF page, take two source pages and place them differently, then close the page.
 # If the source page number is 0, then move on without drawing.
+	Sides = totalPages/2
 	count = 0
 	for n in range(Sides):
 		Quartz.CGContextBeginPage(writeContext, sheetSize)
@@ -155,6 +134,7 @@ def main(argv):
 			
 	# Do tidying up
 	contextDone(writeContext)	
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
