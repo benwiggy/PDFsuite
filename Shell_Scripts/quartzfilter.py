4d7862320af9b66e9592a679d16505ffb1c53045
benwiggy <ben.bw@me.com>
2018-02-15 17:06:12 +0000

    Add files via upload

--- Shell_Scripts/quartzfilter.py
+++ Shell_Scripts/quartzfilter.py
@@ -1,15 +1,28 @@
 #! /usr/bin/python
 # coding: utf-8
 
-# QUARTZFILTER: Script to apply a MacOS Quartz Filter to a PDF file.
-# by Ben Byram-Wigfield v.1.2
+# QUARTZFILTER  v.1.3: Script to apply a MacOS Quartz Filter to a PDF file.
+# by Ben Byram-Wigfield
 # 
 # quartzfilter.py <input <filter> <output>
 #
+# The script will check the folders for installed filters if file path not given.
+
 import os, getopt, sys
 from Quartz import PDFDocument
 from CoreFoundation import (NSURL, QuartzFilter)
 
+def checkFilter(pathname):
+	if not os.path.split(pathname)[0]:
+		Filters = (Quartz.QuartzFilterManager.filtersInDomains_(None))
+		for eachFilter in Filters:
+			name = eachFilter.url().fileSystemRepresentation()
+			if pathname == os.path.split(name)[1]:
+				return name
+	else:
+		if os.path.exists(pathname):
+			return pathname
+				
 def main(argv):
 	inputfile = ""
 	outputfile = ""
@@ -32,13 +45,15 @@ def main(argv):
 		sys.exit(2)
 
 	filter = args[1].decode('utf-8')
+	filter = checkFilter(filter)
 	if not filter:
-		print 'Unable to create context filter'
+		print 'Unable to find Quartz Filter'
 		sys.exit(2)
 
 	outputfile = args[2].decode('utf-8')
+	# You could just take the inputfile as the outputfile if not explicitly given.
 	if not outputfile:
-		print 'Unable to create output context'
+		print 'No valid output file specified'
 		sys.exit(2)
 
 	pdfURL = NSURL.fileURLWithPath_(inputfile)
