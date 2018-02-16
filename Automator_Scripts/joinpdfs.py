e71d858c571085aefbc09acd0034b45d745120d3
benwiggy <ben.bw@me.com>
2018-02-16 11:52:17 +0000

    Add files via upload

--- Automator_Scripts/joinpdfs.py
+++ Automator_Scripts/joinpdfs.py
@@ -1,39 +1,20 @@
 #! /usr/bin/python
 # coding=utf-8
 #
-# Tool to concatenate PDFs.
-# Modified from the Apple script in /System/Library/Automator/Combine PDF Pages.action
-# Optimized for speed (importing * takes many seconds); shuffle and verbose modes removed.
-# Joins all PDF files given as arguments. No sanity checking: if used in Automator, no need.
+# JOINPDFS v1.0 : Tool to concatenate PDFs.
+# New tool built from the ground up using PDFKit, instead of Core Graphics.
+# by Ben Byram-Wigfield
 
 import sys
-import os
-from CoreFoundation import (CFURLCreateFromFileSystemRepresentation, kCFAllocatorDefault)
+import os.path
+from CoreFoundation import (CFURLCreateFromFileSystemRepresentation, kCFAllocatorDefault, NSURL)
 import Quartz as Quartz
 
+
 def createPDFDocumentWithPath(path):
-	return Quartz.CGPDFDocumentCreateWithURL(CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, path, len(path), False))
-
-
-def writePageFromDoc(writeContext, doc, pageNum):
-
-	page = Quartz.CGPDFDocumentGetPage(doc, pageNum)
-	if page:
-		mediaBox = Quartz.CGPDFPageGetBoxRect(page, Quartz.kCGPDFMediaBox)
-		if Quartz.CGRectIsEmpty(mediaBox):
-			mediaBox = None
-
-		Quartz.CGContextBeginPage(writeContext, mediaBox)
-		Quartz.CGContextDrawPDFPage(writeContext, page)
-		Quartz.CGContextEndPage(writeContext)
-
-
-def append(writeContext, docs, maxPages):
-
-	for doc in docs:
-		for pageNum in xrange(1, maxPages + 1) :
-			writePageFromDoc(writeContext, doc, pageNum)
-
+	pdfURL = NSURL.fileURLWithPath_(path)
+	if pdfURL:
+		return Quartz.PDFDocument.alloc().initWithURL_(pdfURL)
 
 def getFilename(filepath, basename):
 	fullname = basename + ".pdf"
@@ -43,36 +24,28 @@ def getFilename(filepath, basename):
 		fullname = basename + " %02d.pdf"%i
 	return os.path.join(filepath, fullname)
 
-
-def main(incomingFiles):
-
-	# Set the output file
+def join(incomingFiles):
+	# Set the output file location and name.
 	prefix = os.path.dirname(incomingFiles[0])
 	filename = "Combined"
 	outfile = getFilename(prefix, filename)
+	# Load in the first PDF file, to which the rest will be added.
+	firstPDF = createPDFDocumentWithPath(incomingFiles[0])
+	pageIndex = firstPDF.pageCount()
 	
-	# The PDF context we will draw into to create a new PDF
-	writeContext = Quartz.CGPDFContextCreateWithURL(CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, outfile, len(outfile), False), None, None)
+	# create PDFDocument object for the remaining files.
+	docs = map(createPDFDocumentWithPath, incomingFiles[1:])
+	for doc in docs:
+		if doc:
+			pages = doc.pageCount()
+			for p in range(0, pages):
+				pageIndex = firstPDF.pageCount()
+				page = doc.pageAtIndex_(p)
+				firstPDF.insertPage_atIndex_(page, pageIndex)
+				
+	firstPDF.writeToFile_(outfile)		
 	
-	if writeContext:
-		# create PDFDocuments for all of the files.
-		docs = map(createPDFDocumentWithPath, incomingFiles)
-		
-		# find the maximum number of pages.
-		maxPages = 0
-		for doc in docs:
-
-			if Quartz.CGPDFDocumentGetNumberOfPages(doc) > maxPages:
-				maxPages = Quartz.CGPDFDocumentGetNumberOfPages(doc)
-	
-		append(writeContext, docs, maxPages)
-		
-		Quartz.CGPDFContextClose(writeContext)
-		del writeContext
-		#CGContextRelease(writeContext)
-
-
-
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	if len(sys.argv) > 1:
+		join(sys.argv[1:])
