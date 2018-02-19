419b3a0cc0e20b6502ca045065f00cacde1a1124
benwiggy <ben.bw@me.com>
2018-02-19 19:24:00 +0000

    Add files via upload

--- Automator_Scripts/joinpdfs.py
+++ Automator_Scripts/joinpdfs.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python
 # coding=utf-8
 #
-# JOINPDFS v2.0 : Tool to concatenate PDFs.
+# JOINPDFS v2.1 : Tool to concatenate PDFs.
 # New tool built from the ground up using PDFKit, instead of Core Graphics.
+# Now writes Table of Contents for each file added.
 # by Ben Byram-Wigfield
 
 import sys
 import os.path
-from CoreFoundation import (CFURLCreateFromFileSystemRepresentation, kCFAllocatorDefault, NSURL)
+from CoreFoundation import (CFURLCreateFromFileSystemRepresentation, kCFAllocatorDefault, NSURL, NSString)
 import Quartz as Quartz
-
 
 def createPDFDocumentWithPath(path):
 	pdfURL = NSURL.fileURLWithPath_(path)
@@ -24,27 +24,49 @@ def getFilename(filepath, basename):
 		fullname = basename + " %02d.pdf"%i
 	return os.path.join(filepath, fullname)
 
+
+def getOutline(page, label, pageObject):
+	pageSize = pageObject.boundsForBox_(Quartz.kCGPDFMediaBox)
+	x = 0
+	y = Quartz.CGRectGetHeight(pageSize)
+	pagePoint = Quartz.CGPointMake(x,y-1)
+	myDestination = Quartz.PDFDestination.alloc().initWithPage_atPoint_(pageObject, pagePoint)
+	myLabel = NSString.stringWithString_(label)
+	myOutline = Quartz.PDFOutline.alloc().init()
+	myOutline.setLabel_(myLabel)
+	myOutline.setDestination_(myDestination)
+	return myOutline
+
 def join(incomingFiles):
+
 	# Set the output file location and name.
 	prefix = os.path.dirname(incomingFiles[0])
 	filename = "Combined"
 	outfile = getFilename(prefix, filename)
 	# Load in the first PDF file, to which the rest will be added.
 	firstPDF = createPDFDocumentWithPath(incomingFiles[0])
-	pageIndex = firstPDF.pageCount()
+	rootOutline = Quartz.PDFOutline.alloc().init()
+	firstLabel = os.path.basename(incomingFiles[0])
+	firstPage = firstPDF.pageAtIndex_(0)
+	firstOutline = getOutline(0, firstLabel, firstPage)
+	rootOutline.insertChild_atIndex_(firstOutline, 0)
 	
 	# create PDFDocument object for the remaining files.
-	docs = map(createPDFDocumentWithPath, incomingFiles[1:])
-	for doc in docs:
-		if doc:
+	pdfObjects = map(createPDFDocumentWithPath, incomingFiles[1:])
+	for index, doc in enumerate(pdfObjects):
+		if doc:	
 			pages = doc.pageCount()
+			pageIndex = firstPDF.pageCount()
+			tocLabel = os.path.basename(incomingFiles[index+1])
 			for p in range(0, pages):
-				pageIndex = firstPDF.pageCount()
 				page = doc.pageAtIndex_(p)
-				firstPDF.insertPage_atIndex_(page, pageIndex)
-				
-	firstPDF.writeToFile_(outfile)		
-	
+				firstPDF.insertPage_atIndex_(page, pageIndex+p)
+				if p == 0:
+					outline = getOutline(pageIndex, tocLabel, page)		
+			rootOutline.insertChild_atIndex_(outline, index+1)
+	# Add the root Outline to the first PDF.
+	firstPDF.setOutlineRoot_(rootOutline)				
+	firstPDF.writeToFile_(outfile)			
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
