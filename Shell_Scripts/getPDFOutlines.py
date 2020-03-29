59021dffcf664e3d7eed5b04d4a408212e5a2c00
benwiggy <ben.bw@me.com>
2020-03-29 21:10:35 +0100

    Add files via upload

--- Shell_Scripts/getPDFOutlines.py
+++ Shell_Scripts/getPDFOutlines.py
@@ -1,9 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# coding=utf-8
+
+# GET PDF OUTLINES v. 1.2
+# by Ben Byram-Wigfield
+# This script will list the Table of Contents data from any PDF file(s) given as arguments.
+# Haven't sorted out indenting text for each hierarchical level.
 
 from Foundation import  NSURL
 import Quartz as Quartz
 import sys
-
+	
+def recurseOutlines(thisOutline):
+	print (thisOutline.label())
+	print (thisOutline.destination())
+	if thisOutline.numberOfChildren() != 0:
+		for n in range(thisOutline.numberOfChildren()):
+			recurseOutlines(thisOutline.childAtIndex_(n))
 
 def getOutlines(infile):
 	pdfURL = NSURL.fileURLWithPath_(infile)
@@ -11,11 +23,11 @@ def getOutlines(infile):
 	if myPDF:
 		outline = Quartz.PDFOutline.alloc().init()
 		outline = myPDF.outlineRoot()
-		for n in range(outline.numberOfChildren()):
-			print n
-			print outline.childAtIndex_(n).label()
-			print outline.childAtIndex_(n).destination()
-		
+		if outline:
+			if outline.numberOfChildren() != 0:
+				for n in range(outline.numberOfChildren()):
+					recurseOutlines(outline.childAtIndex_(n))
+					
 if __name__ == '__main__':
 	for filename in sys.argv[1:]:
 		getOutlines(filename)
